8eb8910ab82d3dabbb8249ca386ca6359c9f7f1b
luis4PF <luishtardem@gmail.com>
2023-03-09 16:11:11 -0300

    add js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timer = {
     pomodoro: 1,
-    shortBreak: 1,
-    longbreak: 15,
+    shortBreak: 5,
+    longBreak: 15,
     longbreakInterval: 4,
     sessions: 0,
 };
@@ -13,11 +13,10 @@ const alterButton = document.getElementById('id-alter');
 
 mainButton.addEventListener('click', () => {
     const { action } = mainButton.dataset;
-    if (action == 'start') {
+    if (action === 'start') {
         startTimer();
         alterButton.classList.add('active')
-    }
-    else {
+    }    else {
         stopTimer();
         alterButton.classList.remove('active')
     }
@@ -34,18 +33,18 @@ function getRemainingTime(endTime) {
     const different = endTime - time;
 
     const total = Number.parseInt(different / 1000, 10);
-    const min = Number.parseInt((total / 60) % 60, 10);
-    const sec = Number.parseInt(total % 60, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
 
     return {
         total,
-        min,
-        sec,
+        minutes,
+        secconds,
     };
 }
 
 function handleTimeChange() {
-    timer.pomodoro = timeInput.ariaValueMax;
+    timer.pomodoro = timeInput.value;
     switchMode(timer.mode);
 }
 
@@ -92,10 +91,80 @@ function startTimer() {
 
             document.querySelector(`[data-sound="${timer.mode}"]`).play();
 
-            counterCount++;
+            count++;
             counter.textContent = count;
 
             startTimer();
         }
     }, 1000);
 }
+
+function stopTimer(){
+    clearInterval(interval);
+
+    mainButton.dataset.action = 'start'
+    mainButton.textContent = 'start';
+    mainButton.classList.remove('active');
+}
+
+function updateClock(){
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.getElementById('id-min');
+    const sec = document.getElementById('id-sec');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+
+    const text = 
+        timer.mode = 'pomodoro' ? 'Volte ao trabalho' : 'Hora de descansar';
+    document.title = `${minutes}:${seconds} - ${text}`;
+}
+
+function switchMode(mode){
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    };
+
+    document.querySelectorAll('button[data-mode]').forEach(e => e.classList.remove('active'));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+    document.body.style.backgroundColor = `var(--${mode})`;
+    document.getElementById('id-progress').setAttribute('max', timer.remainingTime.total);
+
+    updateClock();
+}
+
+function handleMode(event){
+    const { mode } = event.target.dataset;
+
+    if (!mode) return;
+
+    switchMode(mode);
+    stopTimer();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if ('Notification' in window) {
+        if(
+            Notification.permission !== 'granted' &&
+            Notification.permission !== 'denied'
+        ) {
+            Notification.requestPermission().then(function (permission) {
+                if (permission === 'granted') {
+                    new Notification(
+                        'Voce será notificado toda vez que uma sessão iniciar'
+                    );
+                }
+            });
+        }
+    }
+
+    switchMode('pomoodoro');
+});
+
+
+
